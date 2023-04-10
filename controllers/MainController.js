169f83ae6d0e7d642cd4219c2f8c921fce834c88
Faith Griffin <faith_griffin@dlsu.edu.ph>
2023-04-10 14:31:20 +0800

    update: ui display of home page; add: pages for all nodes

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -6,6 +6,21 @@ const MainController = {
         res.render('index');
     },
 
+		getCentralNode: function(req, res) {
+			console.log("Sending central node page.");
+			res.render('node1');
+		},
+
+		getNode2: function(req, res) {
+			console.log("Sending node 2 page.");
+			res.render('node2');
+		},
+
+		getNode3: function(req, res) {
+			console.log("Sending node 3 page.");
+			res.render('node3');
+		},
+
     sampleQuery: function(req, res) {
         var query = `SELECT *
                      FROM movies_normalized
--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const MainController = require('./controllers/MainController');
 
 router.get('/', MainController.getIndex);
+router.get('/node1', MainController.getCentralNode);
+router.get('/node2', MainController.getNode2);
+router.get('/node3', MainController.getNode3);
 router.get('/sample', MainController.sampleQuery);
 
 module.exports = router;
